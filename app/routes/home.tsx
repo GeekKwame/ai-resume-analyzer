457e806fad6b687842c9db7af33dadbc01332331
blessingedmund <dogbeblessingkwame@gmail.com>
2026-01-21 02:53:02 +0000

    feat: integrate Puter.js, add Auth page and ScoreCircle component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -11,15 +14,22 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your resume and get AI-powered feedback to land your dream job</h2>
         </div>
-      </section>
       {resumes.length > 0 && (
       <div className="resumes-section">
       {resumes.map((resume) => (
@@ -29,7 +39,7 @@ export default function Home() {
       </div>
       )}
 
-
+      </section>
     </main>
   );
 }
